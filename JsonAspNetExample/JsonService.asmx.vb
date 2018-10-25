df95b548936cd74309f8e8c390c7ab73946ff308
kinotavr1996 <olegolegovich1996@gmail.com>
2018-10-25 20:35:57 +0300

    initial commit

---
+++ JsonAspNetExample/JsonService.asmx.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Web.Services
+Imports System.Web.Services.Protocols
+Imports System.ComponentModel
+Imports System.Web.Script.Services
+Imports Newtonsoft.Json
+
+' To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+' <System.Web.Script.Services.ScriptService()> _
+<System.Web.Services.WebService(Namespace:="http://tempuri.org/")>
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)>
+<ToolboxItem(False)>
+Public Class JsonService
+    Inherits System.Web.Services.WebService
+
+    <WebMethod()>
+    <ScriptMethod(UseHttpGet:=True, ResponseFormat:=ResponseFormat.Json, XmlSerializeString:=False)>
+    Public Function HelloWorld() As String
+        Return "Hello World"
+    End Function
+
+    <WebMethod()>
+    <ScriptMethod(UseHttpGet:=True, ResponseFormat:=ResponseFormat.Json, XmlSerializeString:=False)>
+    Public Function GetJson() As String
+        Dim example As New Employee
+        With example
+            .Name = "Ivan"
+            .LastName = "Ivanovich"
+            .Age = 45
+            .Country = "Ukraine"
+        End With
+
+        Dim json = JsonConvert.SerializeObject(example)
+        Context.Response.Write(json)
+        Context.Response.End()
+        Return json
+    End Function
+End Class
+
+Public Class Employee
+    Public Name As String
+    Public LastName As String
+    Public Age As Integer
+    Public Country As String
+End Class
---
+++ JsonAspNetExample/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("JsonAspNetExample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("JsonAspNetExample")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("76f537b6-b5c1-4b80-973e-9d6d0de4cdb9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
